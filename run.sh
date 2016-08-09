23501e953e5bf7f96151265755ace516cb516751
TaeL Kim <tael.programmer@gmail.com>
2016-08-09 19:48:46 +0900

    First commit.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # Remember replace USER with your username and REPO with your repository/application name!
 git init
 curl -u 'tael.programmer@gmail.com':'aldksgo122' https://api.github.com/user/repos -d '{"name":"dp"}'
-git remote add origin https://tael.programmer\@gmail.com:aldksgo122@github.com/tael/dp.git
+git remote add origin git@github.com/tael/dp.git
 echo "." > log.md
 git add *
 git config --global user.email "tael.programmer@gmail.com"
